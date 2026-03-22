70b1367efb65447ed02b06045743730494b83e34
clitustsai <infoclituspc@gmail.com>
2026-03-22 13:30:54 +0700

    feat: modern navbar dropdown + fix floating buttons overlap + sw v10

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ========== SERVICE WORKER - PWA ==========
-const CACHE_NAME = 'clituspc-v9';
+const CACHE_NAME = 'clituspc-v10';
 const OFFLINE_URL = '/offline.html';
 
 // Files cần cache để offline
