24f2dc8e91072a4b1bb5783634f881533dc01485
clitustsai <infoclituspc@gmail.com>
2026-03-23 06:53:29 +0700

    fix: bump SW cache v29 để clear cache protect.js cũ

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ========== SERVICE WORKER - PWA ==========
-const CACHE_NAME = 'clituspc-v28';
+const CACHE_NAME = 'clituspc-v29';
 const OFFLINE_URL = '/offline.html';
 
 // Files cần cache để offline
