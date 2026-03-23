7d828a4b6ab155e290cb4d618b693c3bb1ace64b
clitustsai <infoclituspc@gmail.com>
2026-03-23 17:36:31 +0700

    perf: add ?v=2 + defer to features scripts in blog.html and ads.html

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ========== SERVICE WORKER - PWA ==========
-const CACHE_NAME = 'clituspc-v32';
+const CACHE_NAME = 'clituspc-v33';
 const OFFLINE_URL = '/offline.html';
 
 // Files cần cache để offline
